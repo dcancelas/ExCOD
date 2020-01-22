8197ed6d1485e522d0b79d600fd0dbf3849c639f
DanielCancelas <dcancelasgarcia@danielcastelao.org>
2020-01-22 14:06:36 +0100

    B

--- src/excod1av/ExCOD1Av.java
+++ src/excod1av/ExCOD1Av.java
@@ -15,7 +15,7 @@ public class ExCOD1Av {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("Hola mundo");
+        System.out.println("Adiós mundo");
     }
     
 }
